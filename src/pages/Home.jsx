97a369b7b6bfe23d7d73c7382a6fc0a9bcd0e63e
kompella uday <kompellauday9@gmail.com>
2022-10-06 13:57:33 +0530

    githubfinder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults"
+import Usersearch from "../components/users/Usersearch"
+function Home() {
+  return (
+    <>
+        <Usersearch />
+        <UserResults />
+    </>
+  )
+}
+
+export default Home
